8dc8dd5e49ca338c312aac399443d61e25699e42
Deni Simon <denimallel@gmail.com>
2019-06-02 11:27:58 +0530

    initial commit

---
+++ src/main/java/com/deni/web/sailerdocker/SailerDockerApplication.java
@@ -0,0 +1,13 @@
+package com.deni.web.sailerdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SailerDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SailerDockerApplication.class, args);
+    }
+
+}
